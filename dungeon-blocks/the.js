46a180a0330219d6596293b45d48e75c1f855e4d
Little-Jeffy-Joe <119092333+Little-Jeffy-Joe@users.noreply.github.com>
2024-02-13 15:54:50 -0700

    Create the.js

---
+++ dungeon-blocks/the.js
@@ -0,0 +1,15 @@
+document.write('<link rel="stylesheet" href="https://the-dungeoneer.github.io/dungeon-blocks/the.css">');
+const db = {
+create(content){
+document.write(`<div class="block-stack">${content}</div>`);
+},
+block(type,content){
+
+},
+reporter(type,content,boolean_or_reporter){
+
+},
+input(content){
+
+},
+}
