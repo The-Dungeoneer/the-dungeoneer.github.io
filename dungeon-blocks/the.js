3b94f73b468666fafe818e61164c9882e5698b2d
Little-Jeffy-Joe <119092333+Little-Jeffy-Joe@users.noreply.github.com>
2024-09-19 19:00:21 -0600

    Update the.js

--- dungeon-blocks/the.js
+++ dungeon-blocks/the.js
@@ -1,4 +1,11 @@
+/*
+Until this is working properly, it will be kept in a comment.
+because it currently breaks the.css.
+
+
 const dblocks = {};
 dblocks.create = function(content){
 	document.write(`<div class="block-stack">${content}</div>`);
 }
+
+*/
