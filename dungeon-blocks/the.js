9c1a809f31d19a2b0e38c70a73cf095c0e938fce
Little-Jeffy-Joe <119092333+Little-Jeffy-Joe@users.noreply.github.com>
2024-09-19 18:57:23 -0600

    Update the.js

--- dungeon-blocks/the.js
+++ dungeon-blocks/the.js
@@ -1,4 +1,3 @@
-document.write(`<link rel="stylesheet" href="https://the-dungeoneer.github.io/dungeon-blocks/the.css">`);
 const dblocks = {};
 dblocks.create = function(content){
 	document.write(`<div class="block-stack">${content}</div>`);
